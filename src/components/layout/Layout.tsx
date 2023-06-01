da0aa3dea966e1231f28d1540e4ccdc87d764c22
mustafa badan <62083173+mustafabadan1@users.noreply.github.com>
2023-06-01 17:20:37 +0300

    message

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 
+import Header from '@/components/layout/Header';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // Put Header or Footer Here
-  return <>
-  
-  <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-  {children}</>;
+  //  Header or Footer Here
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
 }
